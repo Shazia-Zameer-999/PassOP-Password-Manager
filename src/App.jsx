6b8dbd0cb7c3a44d634757e86d16915436744bd2
Daten by SZ <datendiva7867@gmail.com>
2025-10-31 13:39:35 +0530

    project 4'

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import Manager from './components/manager'
+import Manager from './components/Manager'
 import Footer from './components/Footer'
 
 function App() {
